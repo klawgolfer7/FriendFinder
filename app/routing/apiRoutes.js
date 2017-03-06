3e9fd3863357593a7014f6711df452de8d62526b
Kevin Lawler <kevinrlawler@gmail.com>
2017-03-05 21:34:23 -0500

    started apiRoutes file

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+app.get("../survey", function(req, res) {
+
+	
+}

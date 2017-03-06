cfa5be4488f581ca563f0029b7625d1a77dc97e0
Kevin Lawler <kevinrlawler@gmail.com>
2017-03-05 21:11:44 -0500

    updated array in server.js

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+var port = 3000;
+
+var names = [
+	{
+		name: "Kevin Lawler",
+		photo: "https://www.facebook.com/photo.php?fbid=10106275589026100&set=a.10100245501190460.2864996.1930710&type=3&theater",
+		scores: [
+		5,
+		1,
+		4,
+		3,
+		5,
+		2,
+		4,
+		5,
+		3,
+		1
+		]
+	}
+
+];
+
+
+
+
+
+
+
+
+

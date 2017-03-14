ba28ac86ecb5039467f901e6b7a63a5455581136
klawgolfer7 <kevinrlawler@gmail.com>
2017-03-13 20:58:27 -0400

    adding friends.js file

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+var friendList = [{
+	"name" : "Kevin",
+	"photo" : "https://www.linkedin.com/in/kevin-lawler-70ab1b18/edit/topcard/",
+	"scores" :[3,3,5,1,2,4,3,3,2,3]
+}]
+
+module.exports = friendList;
